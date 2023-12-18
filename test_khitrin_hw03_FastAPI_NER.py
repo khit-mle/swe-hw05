af81efefb9b4e0c4fb652cc39a146f5df76a31b3
khit-mle <149804920+khit-mle@users.noreply.github.com>
2023-12-18 07:36:39 +0500

    fix conseq post task and get results requests
    Update test_khitrin_hw03_FastAPI_NER.py

--- test_khitrin_hw03_FastAPI_NER.py
+++ test_khitrin_hw03_FastAPI_NER.py
@@ -12,32 +12,39 @@ def test_root_redirects_to_docs():
 
 def test_extract_org_entities():
     text_for_extrct = "Google and YouTube are the two most visited websites followed by Facebook."
-    response = client.post("/api/v1/ner/tasks",
+    create_task_response = client.post("/api/v1/ner/tasks",
                            json={"text_for_extraction": text_for_extrct})
-    json_data = response.json()
-    assert response.status_code == 200
-    assert json_data['status'] == 'done'
-    assert json_data['input']['text_for_extraction'] == text_for_extrct
-    assert json_data['output'][0]['entity'] == 'B-ORG'
-    assert json_data['output'][0]['word'] == 'Google'
-    assert json_data['output'][1]['entity'] == 'B-ORG'
-    assert json_data['output'][1]['word'] == 'YouTube'
-    assert json_data['output'][2]['entity'] == 'B-ORG'
-    assert json_data['output'][2]['word'] == 'Facebook'
+    create_task_json_data = create_task_response.json()
+    assert create_task_response.status_code == 202
+    task_id = create_task_json_data['task_id']
+    read_result_response = client.get(f"/api/v1/ner/tasks/{task_id}")
+    read_result_response_json_data = read_result_response.json()
+    assert read_result_response.status_code == 200
+    assert read_result_response_json_data['status'] == 'done'
+    assert read_result_response_json_data['input']['text_for_extraction'] == text_for_extrct
+    assert read_result_response_json_data['output'][0]['entity'] == 'B-ORG'
+    assert read_result_response_json_data['output'][0]['word'] == 'Google'
+    assert read_result_response_json_data['output'][1]['entity'] == 'B-ORG'
+    assert read_result_response_json_data['output'][1]['word'] == 'YouTube'
+    assert read_result_response_json_data['output'][2]['entity'] == 'B-ORG'
+    assert read_result_response_json_data['output'][2]['word'] == 'Facebook'
 
 
 def test_extract_loc_entities():
     text_for_extrct = "Dublin is the capital of Ireland which is in Europe."
-    response = client.post("/api/v1/ner/tasks",
+    create_task_response = client.post("/api/v1/ner/tasks",
                            json={"text_for_extraction": text_for_extrct})
-    json_data = response.json()
-    assert response.status_code == 200
-    assert json_data['status'] == 'done'
-    assert json_data['input']['text_for_extraction'] == text_for_extrct
-    assert json_data['output'][0]['entity'] == 'B-LOC'
-    assert json_data['output'][0]['word'] == 'Dublin'
-    assert json_data['output'][1]['entity'] == 'B-LOC'
-    assert json_data['output'][1]['word'] == 'Ireland'
-    assert json_data['output'][2]['entity'] == 'B-LOC'
-    assert json_data['output'][2]['word'] == 'Europe'
-  
+    create_task_json_data = create_task_response.json()
+    assert create_task_response.status_code == 202
+    task_id = create_task_json_data['task_id']
+    read_result_response = client.get(f"/api/v1/ner/tasks/{task_id}")
+    read_result_response_json_data = read_result_response.json()
+    assert read_result_response.status_code == 200
+    assert read_result_response_json_data['status'] == 'done'
+    assert read_result_response_json_data['input']['text_for_extraction'] == text_for_extrct
+    assert read_result_response_json_data['output'][0]['entity'] == 'B-LOC'
+    assert read_result_response_json_data['output'][0]['word'] == 'Dublin'
+    assert read_result_response_json_data['output'][1]['entity'] == 'B-LOC'
+    assert read_result_response_json_data['output'][1]['word'] == 'Ireland'
+    assert read_result_response_json_data['output'][2]['entity'] == 'B-LOC'
+    assert read_result_response_json_data['output'][2]['word'] == 'Europe'  
